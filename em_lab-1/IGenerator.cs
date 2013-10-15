11f6f4c23a495e9be8b36bdfd859797fe0b4f9c3
Roman Bietin <diez.ukr@gmail.com>
2013-10-15 19:02:21 +0300

    redesign

--- em_lab-1/IGenerator.cs
+++ em_lab-1/IGenerator.cs
@@ -8,8 +8,6 @@ namespace em_lab_1
 {
     interface IGenerator
     {
-        protected long seed;
-        protected long x;
         public long getSeed();
         public long getNext();
         public IGenerator(long seed);
