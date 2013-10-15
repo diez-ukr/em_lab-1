7996ea864f58600ee09969765431227a6ac39466
Roman Bietin <diez.ukr@gmail.com>
2013-10-15 18:33:22 +0300

    initial commit with interface for generators

---
+++ em_lab-1/IGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace em_lab_1
+{
+    interface IGenerator
+    {
+        protected long seed;
+        protected long x;
+        public long getSeed();
+        public long getNext();
+        public IGenerator(long seed);
+    }
+}
