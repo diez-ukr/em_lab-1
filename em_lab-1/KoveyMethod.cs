7c88870dd76bb9c1a1e87a11e7079c297492f771
Pyrozhok Vladyslav <headway1992@gmail.com>
2013-10-16 22:11:43 +0300

    KoveyMethod added

---
+++ em_lab-1/KoveyMethod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace em_lab_1
+{
+    class KoveyMethod : IGenerator
+    {
+        private long x;
+        private long seed;
+        private int l;
+
+        public KoveyMethod(long seed, int l)
+        {
+            this.x = seed;
+            this.seed = seed;
+            this.l = l;
+        }
+
+        public long getNext()
+        {
+            this.x = this.x * (this.x + 1) % (long)Math.Pow(2, l);
+            return this.x;
+        }
+
+        public long getSeed()
+        {
+            return seed;
+        }
+    }
+}
