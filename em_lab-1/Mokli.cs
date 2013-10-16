229a340107d6bcc366fe2d5955aec422db169fea
Zh04 <zheka04@gmail.com>
2013-10-17 01:38:09 +0300

    Update Mokli.cs

--- em_lab-1/Mokli.cs
+++ em_lab-1/Mokli.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace LabChM1
+namespace em_lab-1
 {
     class Mokli: IGenerator
     {
         public List<int> newList = new List<int>();
-        FonNeymon fonneymon = new FonNeymon();
+        FonNeymon fonneymon;
+        long val1;
+        long val6;
         private long next;
 
         public Mokli(int value1, int value6)
         {
-            next = nextRand(value1, value6);
+            fonneymon = new FonNeymon(value1, value6, 5);
+            val1 = value1;
+            val6 = value6;
         }
         public int nextRand(int value1,int value6)
         {
-            long newValue=fonneymon.nextRandom(value1, value6);
+            long newValue=fonneymon.getNext();
             return (int)newValue;
         }
         public long getSeed()
@@ -26,6 +30,7 @@ namespace LabChM1
         }
         public long getNext()
         {
+            next = nextRand((int)val1, (int)val6);
             return next;
         }
     }
